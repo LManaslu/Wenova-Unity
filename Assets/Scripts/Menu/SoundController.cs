a3447c81ef4a029141cff62962979b332b26c907
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2019-03-11 17:19:29 -0300

    #1 Making musics continues

---
+++ Assets/Scripts/Menu/SoundController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+    public static SoundController instance;
+
+    void Awake() {
+        if(instance == null) instance = this;
+        else Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+}
