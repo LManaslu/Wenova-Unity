443f7c9c70766e721e987da9b56db36913deabaf
Joao Paulo Busche <joaopaulo.busche@gmail.com>
2019-06-15 14:45:15 -0300

    #3 Adding animation for characters

--- Assets/Scripts/CharacterSelector/PlayerSelector.cs
+++ Assets/Scripts/CharacterSelector/PlayerSelector.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class P1Selector : MonoBehaviour {
+public class PlayerSelector : MonoBehaviour {
 
     public GameObject[] positions;
     public Sprite imageNum;
@@ -11,16 +11,19 @@ public class P1Selector : MonoBehaviour {
 
     int index = 0;
     DataController data;
+    Animator myAnim;
     // Start is called before the first frame update
     void Start() {
         index = 0;
         data = FindObjectOfType<DataController>();
+        myAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update() {
         positions[index].GetComponent<SpriteRenderer>().sprite = imageNum;
         charText.text = data.characters[index];
+        myAnim.Play(data.characters[index]);
         if(Input.GetKeyDown(KeyCode.RightArrow) && index < positions.Length - 1) {
             positions[index].GetComponent<SpriteRenderer>().sprite = null;
             index++;
